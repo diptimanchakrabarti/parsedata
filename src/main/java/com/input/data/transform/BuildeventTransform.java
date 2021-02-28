a9637fb15c117914ff92e59f4e0508d6ecfed185
Diptiman Chakrabarti <diptiman.chakrabarti@in.ibm.com>
2021-02-27 19:31:27 -0500

    added codes

---
+++ src/main/java/com/input/data/transform/BuildeventTransform.java
@@ -0,0 +1,15 @@
+package com.input.data.transform;
+
+import io.connectedhealth_idaas.eventbuilder.events.platform.RoutingEvent;
+import io.connectedhealth_idaas.eventbuilder.parsers.clinical.FHIRStreamParser;
+
+public class BuildeventTransform {
+
+ public String returnFHIRAllergy(String msgBody) {
+     FHIRStreamParser parser = new FHIRStreamParser();
+     RoutingEvent msgRoutingEvent = parser.buildRoutingEvent(msgBody);
+     return msgRoutingEvent.toString();
+    }
+
+
+}
